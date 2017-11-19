c57cf151ca8fb2f8bbd05910e2b1e7d7a6304be6
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2017-11-19 19:43:01 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,6 +1,6 @@
 function IcebreakerResponse(email){
   this.email = email
 }
-IcebreakerResponse.BatchCreate(email){
+IcebreakerResponse.BatchCreate(){
 return emails.map(function(email)
 { return new IcebreakerResponse(email) }}
